a00663ab20a9820c94da05fd0e33ca256c89dab7
Rodrigo Lomar de Albuquerque <rodrigolomardev@gmail.com>
2023-01-24 14:48:41 -0300

    conserta metodo register

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,19 +6,24 @@ exports.index = (req, res) => {
 };
 
 exports.register = async (req, res) => {
-    const login = new Login(req.body);
-    await login.register();
-
-    if(!login.body.email || !login.body.password) return res.redirect('/login'); 
-
-
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(() => {
-            return res.redirect('/login');
-        });
-        return;
+    try{
+        const login = new Login(req.body);
+        await login.register();
+    
+        if(!login.body.email || !login.body.password) return res.redirect('/login'); 
+    
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => {
+                return res.redirect('/login');
+            });
+            return;
+        }
+    
+        res.send('Em viado');
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
     }
-
-    res.send('Em viado');
 };
