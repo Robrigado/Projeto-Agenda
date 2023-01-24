69f0346e90a697512a6fb4cc41bd1762d1eb4e01
Rodrigo Lomar de Albuquerque <rodrigolomardev@gmail.com>
2023-01-24 16:40:19 -0300

    insere função userExists

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, require: true },
@@ -20,12 +21,26 @@ class Login {
         
         if(this.errors.length > 0) return;
 
+        await this.userExists();
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
             this.user = await LoginModel.create(this.body);
         } catch(e) {
             console.log(e);
         }
     };
+
+    async userExists() {
+        try {
+            const user = await LoginModel.findOne({email: this.body.email})
+            if(user) this.errors.push('Usuário já cadastrado.');
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     validate() {
         this.cleanUp();
